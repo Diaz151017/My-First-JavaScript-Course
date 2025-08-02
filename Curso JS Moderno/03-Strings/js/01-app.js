a1e83de6615a1e98e446d2bf65c89df6876732c8
Diaz151017 <diazoliver151017@gmail.comgit config user.email diazoliver151017@gmail.com>
2025-08-02 13:44:19 +0200

    Declaración de strings Básicas

---
+++ Curso JS Moderno/03-Strings/js/01-app.js
@@ -0,0 +1,10 @@
+const producto = "Monitor 20 pulgadas\"";// Crear una variable const con su string básico const producto = ""; 
+//Si necesitas un string con comillas dobles, puedes usar la barra invertida (\) para escaparlas.
+const producto2 = String('Monitor 24 pulgadas');// Crear una variable const con su string 2º modo
+
+const producto3 = new String('Monitor 32 pulgadas');// Crear una variable const con su string 3º modo (poco común)
+
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
