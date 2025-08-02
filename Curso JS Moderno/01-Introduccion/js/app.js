fb577a69e311935cd0023acbbfb0af09ae7f7da3
Diaz151017 <diazoliver151017@gmail.comgit config user.email diazoliver151017@gmail.com>
2025-08-02 11:23:03 +0200

    Final de introducción al curso

--- Curso JS Moderno/01-Introduccion/js/app.js
+++ Curso JS Moderno/01-Introduccion/js/app.js
@@ -1,8 +1,9 @@
-/*Pregunta al usuario su nombre*/
-const nombre = prompt("¿Cual es tu nombre?");
+console.log("¡Hola");
+console.log("Mundo!");
 
-//Toma el nomnbre y lo muestra en pantalla
-document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript Moderno`;
 
-//Asigna el hacia la variable producto
-const producto = 'Monitor 24 pulgadas';
+function hola() {
+    console.log("Ok");
+    console.log("Hola");
+    console.log("Mundo");
+}
