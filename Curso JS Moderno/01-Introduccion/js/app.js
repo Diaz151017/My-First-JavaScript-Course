3d16b90a427be08e4686d4a180dacd756f10462e
Diaz151017 <diazoliver151017@gmail.comgit config user.email diazoliver151017@gmail.com>
2025-08-02 11:03:40 +0200

    formas de comentar tu código

--- Curso JS Moderno/01-Introduccion/js/app.js
+++ Curso JS Moderno/01-Introduccion/js/app.js
@@ -1,1 +1,8 @@
-alert("Hola Mundo!");
+/*Pregunta al usuario su nombre*/
+const nombre = prompt("¿Cual es tu nombre?");
+
+//Toma el nomnbre y lo muestra en pantalla
+document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript Moderno`;
+
+//Asigna el hacia la variable producto
+const producto = 'Monitor 24 pulgadas';
