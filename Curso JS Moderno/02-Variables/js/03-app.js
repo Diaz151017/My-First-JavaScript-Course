2e7308efed042cab08b71210a8548048b2678f2a
Diaz151017 <diazoliver151017@gmail.comgit config user.email diazoliver151017@gmail.com>
2025-08-02 13:14:59 +0200

    Conceptos básicos de variables 2

---
+++ Curso JS Moderno/02-Variables/js/03-app.js
@@ -0,0 +1,9 @@
+const producto = "Tablet"; // Declaración de variable con let
+//producto = "Monitor"; // Reasignación de variable
+
+console.log(producto); // Imprime "Monitor"
+//const no permite la reasignación de variables, por lo que el código anterior generaría un error si se intentara ejecutar.
+
+const precio = 20;//const siempre debe ser inicializada con un valor, no se puede declarar sin asignar un valor.
+// Declaración de variable con const
+console.log(precio); // Imprime 20
