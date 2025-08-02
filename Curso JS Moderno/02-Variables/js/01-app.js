bd944ab9c11c6c42f2f5b62682a2941a13a97da5
Diaz151017 <diazoliver151017@gmail.comgit config user.email diazoliver151017@gmail.com>
2025-08-02 12:02:32 +0200

    Conceptos básicos de variables 1

---
+++ Curso JS Moderno/02-Variables/js/01-app.js
@@ -0,0 +1,35 @@
+var producto = "Monitor 24 pulgadas";// Declaración de variable
+console.log(producto); // Imprime "Monitor 24 pulgadas"
+
+producto = "Monitor 20 pulgadas"; // Reasignación de variable
+console.log(producto); // Imprime "Monitor 20 pulgadas"
+
+producto = 20; // Reasignación de variable a un número
+console.log(producto); // Imprime 20
+
+//Javascript es un lenguaje de tipado dinámico, por lo que no es necesario declarar el tipo de dato al crear una variable.
+
+//Se incializar sin valor y asignar después
+
+var disponible; // Declaración de variable sin valor
+
+disponible = true; // Asignación de valor
+disponible = false; // Reasignación de valor
+
+//Inicializar múltiples variables.
+    categoria = "Computadoras",
+    marca = "MSI",
+    calificacion = 5; // Se pueden usar comas para declarar múltiples variables en una sola línea.
+
+/*var 99días; // No se puede iniciar una variable con un número, pero se puede usar un número después de una letra o guion bajo.
+var 01_;
+var días99;
+var _01;
+
+var nombreProducto;// Camel case, se usa para variables con múltiples palabras.
+var nombre_producto_categoria_precio; // Snake case, se usa para variables con múltiples palabras separadas por guiones bajos.
+var NombreProducto; // Pascal case, se usa para clases en JavaScript.
+
+//Pascal case es el método más utilizado para nombrar clases en JavaScript, mientras que camel case es común para variables y funciones.
+ 
+*/
---
+++ Curso JS Moderno/02-Variables/js/02-app.js
@@ -0,0 +1,5 @@
+//No se recomienda usar var, es mejor usar let o const para declarar variables en JavaScript moderno.
+
+let producto = "Tablet";//let permite declarar variables que pueden ser reasignadas.
+
+producto = "Monitor"; // Reasignación de variable.
