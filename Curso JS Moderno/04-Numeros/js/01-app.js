328c26d1ca386214dee618597608fa6099029104
Diaz151017 <diazoliver151017@gmail.comgit config user.email diazoliver151017@gmail.com>
2025-08-03 14:33:42 +0200

    Números, números en strings y calculos básicos en variables

---
+++ Curso JS Moderno/04-Numeros/js/01-app.js
@@ -0,0 +1,18 @@
+
+
+
+const number1 = 20;// Número.
+const number2 = 20.2;// Número decimal.
+const number3 = .102030;// Número decimal con punto.
+//No hace falta el uso de comillas para definir un número. Ni especificsar el tipo de dato.
+const number4 = -20;// Incluso se pueden usar números negativos.
+// Los números pueden ser enteros o decimales, positivos o negativos. Pero todos se crean igual, sin comillas.
+
+console.log(number1);
+console.log(number2);
+console.log(number3);
+console.log(number4);
+
+const number5 = "20";// Número mediante string.
+
+console.log(number5); // Esto es un string, no un número.
---
+++ Curso JS Moderno/04-Numeros/js/02-app.js
@@ -0,0 +1,28 @@
+const number1 = 30;
+const number2 = 20;
+
+
+let result;
+
+result1 = number1 + number2; // Add, Suma.
+
+result2 = number1 - number2; // Subtract, Resta.
+
+result3 = number1 * number2; // Multiply, Multiplicación.
+
+result4 = number1 / number2; // Divide, División.
+
+result5 = number1 % number2; // Modulus, Módulo.
+
+
+
+
+
+
+
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
