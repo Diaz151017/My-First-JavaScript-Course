91cdd884eee2893a83f9465d429d5758616457ba
Diaz151017 <diazoliver151017@gmail.comgit config user.email diazoliver151017@gmail.com>
2025-08-04 20:48:49 +0200

    Diferentes modelos de calculo

---
+++ Curso JS Moderno/04-Numeros/js/04-app.js
@@ -0,0 +1,12 @@
+
+let result;
+
+result = 20 + 30 * 2;// Según la jerarquía de operaciones, la multiplicación se realiza antes que la suma.
+
+result = (20 + 30) * 2; // Si se usan paréntesis, la suma se realiza primero.
+
+result = (20 + 30 + 40 + 50+ 60) * .2; // Se puede usar un número decimal para obtener un porcentaje.
+
+result = (20 + 30) *1.16; // Se puede usar un número decimal para aplicar un IVA del 16%.
+
+console.log(result);
