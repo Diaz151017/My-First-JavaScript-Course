8c8daded0544e31a22d819d314c074d950f562a5
Diaz151017 <diazoliver151017@gmail.comgit config user.email diazoliver151017@gmail.com>
2025-08-16 19:29:07 +0200

    Booleans 1

---
+++ Curso JS Moderno/06-Booleans/js/01-app.js
@@ -0,0 +1,10 @@
+const boolean1 = true;
+const boolean2 = false;
+const boolean3 = "true";//Esto no es un booleano, es una cadena de texto.
+
+//console.log(boolean1); // true
+//console.log(boolean2); // false
+
+//console.log(typeof boolean1); // "boolean"
+
+console.log(boolean1 == boolean3); 
