de6a65b280b7c809ba4d586c938153440e1bcd8f
Diaz151017 <diazoliver151017@gmail.comgit config user.email diazoliver151017@gmail.com>
2025-08-17 17:54:16 +0200

    Arrays 02

---
+++ Curso JS Moderno/08-Arrays/js/02-app.js
@@ -0,0 +1,8 @@
+const numbers = [10, 20, 30, 40, 50];
+
+console.log(numbers);
+console.table(numbers); // Utilizando console.table para mostrar el array de forma tabular.
+
+
+console.log(numbers[2]); // Accediendo al tercer elemento del array (índice 2).
+console.log(numbers[0]);
