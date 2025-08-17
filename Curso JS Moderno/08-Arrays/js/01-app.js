652ffc2c14b7d17079180724c1999f5bb8fe3405
Diaz151017 <diazoliver151017@gmail.comgit config user.email diazoliver151017@gmail.com>
2025-08-17 14:29:08 +0200

    Arrays 01

---
+++ Curso JS Moderno/08-Arrays/js/01-app.js
@@ -0,0 +1,12 @@
+const numbers = [10];//Esto es un array se diferencia de un objeto por que no tiene propiedades solo valores separados por comas.
+//Además de utilizar corchetes para definirlo en lugar de llaves.
+const months = new Array('Enero', 'Febrero', 'Marzo');//Esto es otra forma de crear un array, menos común, pero válida.
+
+
+console.log(numbers);
+console.log(months);
+
+
+const mixed = ["Hola", 10, true, "si", null, { name: 'Adrián', work: 'Programador'}, [1, 2, 3]];
+//Un array es una variable que puede contener múltiples valores, incluso de diferentes tipos de datos.
+console.log(mixed);
